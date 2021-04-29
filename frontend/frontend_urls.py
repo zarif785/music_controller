501b9bf8bf21f5ce917541b7b69ddadafc93c168
zarif785 <63856140+zarif785@users.noreply.github.com>
2021-04-29 12:38:52 +0600

    Initial commit

---
+++ frontend/frontend_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .frontend_view_controller import index
+
+app_name = 'frontend'
+
+urlpatterns = [
+    path('', index, name=''),
+    path('join', index),
+    path('create', index),
+    path('room/<str:roomCode>', index)
+]
